eba095bbc2ed82b0b12f6b50ddffb0ea65ee5e0f
Harry Slatyer <harry.slatyer@gmail.com>
2016-05-22 21:47:07 +1000

    Fix up clickability of group list. Now items can be clicked (and they get highlighted when clicked).

--- src/main/java/au/com/museumvictoria/fieldguide/vic/fork/ui/fragments/SpeciesGroupListFragment.java
+++ src/main/java/au/com/museumvictoria/fieldguide/vic/fork/ui/fragments/SpeciesGroupListFragment.java
@@ -26,7 +26,7 @@ import au.com.museumvictoria.fieldguide.vic.fork.util.Utilities;
  * Displays a list of groups.
  */
 public class SpeciesGroupListFragment extends Fragment {
-	private static final String TAG = SpeciesGroupListFragment.class.getSimpleName();
+  private static final String TAG = SpeciesGroupListFragment.class.getSimpleName();
 
   /**
    * Callback interface to be notified when a group is selected. Activities using this fragment
@@ -38,11 +38,11 @@ public class SpeciesGroupListFragment extends Fragment {
 
   private Callback callback;
 
-	private ListView mListView;
-	private Cursor mCursor;
-	private FieldGuideDatabase database;
-	private SpeciesCursorAdapter mAdapter; 
-	
+  private ListView mListView;
+  private Cursor mCursor;
+  private FieldGuideDatabase database;
+  private SpeciesCursorAdapter mAdapter;
+
   @Override
   public void onAttach(Activity activity) {
     super.onAttach(activity);
@@ -55,112 +55,117 @@ public class SpeciesGroupListFragment extends Fragment {
     }
   }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_species_group_list, container, false);
-	}
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_species_group_list, container, false);
+  }
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		
-		database = FieldGuideDatabase.getInstance(getActivity().getApplicationContext());
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
 
-		Log.i(TAG, "Loading grouped items");
+    database = FieldGuideDatabase.getInstance(getActivity().getApplicationContext());
 
-		mCursor = database.getSpeciesGroups();
-		
-		mListView = (ListView) getView().findViewById(R.id.group_list);
-		mListView.setFastScrollEnabled(true);
-		mAdapter = new SpeciesCursorAdapter(getActivity().getApplicationContext(), mCursor, 0);
-		mListView.setAdapter(mAdapter);
+    Log.i(TAG, "Loading grouped items");
 
-		Log.i(TAG, "Done loading items");
-	}
-	
-	@Override
-	public void onDestroy() {
-		mCursor.close();
-		database.close();
-		
-		super.onDestroy();
-	}
+    mCursor = database.getSpeciesGroups();
+
+    mListView = (ListView) getView().findViewById(R.id.group_list);
+    mListView.setFastScrollEnabled(true);
+    mAdapter = new SpeciesCursorAdapter(getActivity().getApplicationContext(), mCursor, 0);
+    mListView.setAdapter(mAdapter);
+    mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "Click" + position + " " + mAdapter.getItem(position));
+        callback.onGroupSelected(mAdapter.getGroupLabelAtPosition(position));
+      }
+    });
+
+    Log.i(TAG, "Done loading items");
+  }
+
+  @Override
+  public void onDestroy() {
+    mCursor.close();
+    database.close();
+
+    super.onDestroy();
+  }
 
   @Override
   public void onDetach() {
     callback = null;
   }
-	
-	private final class SpeciesCursorAdapter extends CursorAdapter implements SectionIndexer {
-		private AlphabetIndexer mAlphabetIndexer;
 
-		public SpeciesCursorAdapter(Context context, Cursor c, int flags) {
-			super(context, c, flags);
+  private static final class SpeciesCursorAdapter extends CursorAdapter implements SectionIndexer {
+    private final AlphabetIndexer mAlphabetIndexer;
 
-			mAlphabetIndexer = new AlphabetIndexer(c, c.getColumnIndex(FieldGuideDatabase.SPECIES_GROUP),
-					" ABCDEFGHIJKLMNOPQRTSUVWXYZ");
-			mAlphabetIndexer.setCursor(c);
-		}
+    public SpeciesCursorAdapter(Context context, Cursor c, int flags) {
+      super(context, c, flags);
 
-		@Override
-		public int getPositionForSection(int section) {
-			return mAlphabetIndexer.getPositionForSection(section);
-		}
+      mAlphabetIndexer = new AlphabetIndexer(c, c.getColumnIndex(FieldGuideDatabase.SPECIES_GROUP),
+          " ABCDEFGHIJKLMNOPQRTSUVWXYZ");
+      mAlphabetIndexer.setCursor(c);
+    }
 
-		@Override
-		public int getSectionForPosition(int position) {
-			return mAlphabetIndexer.getSectionForPosition(position);
-		}
+    @Override
+    public int getPositionForSection(int section) {
+      return mAlphabetIndexer.getPositionForSection(section);
+    }
 
-		@Override
-		public Object[] getSections() {
-			return mAlphabetIndexer.getSections();
-		}
+    @Override
+    public int getSectionForPosition(int position) {
+      return mAlphabetIndexer.getSectionForPosition(position);
+    }
 
-		@Override
-		public void bindView(View view, Context context, Cursor cursor) {
-			final String groupLabel =
-        cursor.getString(cursor.getColumnIndex(FieldGuideDatabase.SPECIES_GROUP));
-			String iconLabel = groupLabel.toLowerCase().replaceAll(" ", "").replaceAll(",", "");
-			String iconPath = Utilities.SPECIES_GROUPS_PATH + iconLabel + ".png"; 
-			
-			TextView txtView1 = (TextView) view.findViewById(R.id.speciesLabel);
-			txtView1.setText(groupLabel);
-			
-			ImageView imgView = (ImageView) view.findViewById(R.id.speciesIcon);
-			// imgView.setImageBitmap(ImageResizer.decodeSampledBitmapFromAsset(getActivity().getAssets(), iconPath, 75, 75));
-	        
-			Log.w(TAG, "Getting AssetsFileDescriptor for species group icon: " + iconPath);
-//			InputStream istr = null;
-//			try {
-//				istr = Utilities.getAssetInputStream(getActivity(), iconPath);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			imgView.setImageBitmap(ImageResizer.decodeSampledBitmapFromStream(istr, 75, 75));
-			imgView.setImageBitmap(ImageResizer.decodeSampledBitmapFromFile(Utilities.getFullExternalDataPath(getActivity(), iconPath), 75, 75));
+    @Override
+    public Object[] getSections() {
+      return mAlphabetIndexer.getSections();
+    }
 
-			
-			TextView txtView2 = (TextView) view.findViewById(R.id.speciesSublabel);
-			// txtView2.setText(cursor.getString(cursor.getColumnIndex(FieldGuideDatabase.SPECIES_SUBLABEL)));
-			txtView2.setVisibility(View.GONE);
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+      String groupLabel = getGroupLabel(cursor);
+      String iconLabel = groupLabel.toLowerCase().replaceAll(" ", "").replaceAll(",", "");
+      String iconPath = Utilities.SPECIES_GROUPS_PATH + iconLabel + ".png";
 
-      view.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          Log.i(TAG, "View clicked: " + v);
-          callback.onGroupSelected(groupLabel);
-        }
-      });
-		}
+      TextView txtView1 = (TextView) view.findViewById(R.id.speciesLabel);
+      txtView1.setText(groupLabel);
 
-		@Override
-		public View newView(Context context, Cursor cursor, ViewGroup parent) {
-			LayoutInflater inflater = LayoutInflater.from(context);
-			View newView = inflater.inflate(R.layout.species_list, parent, false);
-			return newView;
-		}
+      ImageView imgView = (ImageView) view.findViewById(R.id.speciesIcon);
+      // imgView.setImageBitmap(ImageResizer.decodeSampledBitmapFromAsset(getActivity().getAssets(), iconPath, 75, 75));
 
-	}
-	
+      Log.w(TAG, "Getting AssetsFileDescriptor for species group icon: " + iconPath);
+//      InputStream istr = null;
+//      try {
+//        istr = Utilities.getAssetInputStream(getActivity(), iconPath);
+//      } catch (Exception e) {
+//        e.printStackTrace();
+//      }
+//      imgView.setImageBitmap(ImageResizer.decodeSampledBitmapFromStream(istr, 75, 75));
+      imgView.setImageBitmap(ImageResizer.decodeSampledBitmapFromFile(
+            Utilities.getFullExternalDataPath(context, iconPath), 75, 75));
+
+      TextView txtView2 = (TextView) view.findViewById(R.id.speciesSublabel);
+      // txtView2.setText(cursor.getString(cursor.getColumnIndex(FieldGuideDatabase.SPECIES_SUBLABEL)));
+      txtView2.setVisibility(View.GONE);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+      LayoutInflater inflater = LayoutInflater.from(context);
+      View newView = inflater.inflate(R.layout.species_list, parent, false);
+      return newView;
+    }
+
+    public String getGroupLabelAtPosition(int position) {
+      return getGroupLabel((Cursor) getItem(position));
+    }
+
+    private static String getGroupLabel(Cursor cursor) {
+      return cursor.getString(cursor.getColumnIndex(FieldGuideDatabase.SPECIES_GROUP));
+    }
+  }
 }
