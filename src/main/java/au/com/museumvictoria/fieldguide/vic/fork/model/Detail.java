a607feaeedf332020301de407cd7cdc8f978b15e
Harry Slatyer <harry.slatyer@gmail.com>
2016-06-07 23:03:18 +1000

    Fix indentation of Detail.

--- src/main/java/au/com/museumvictoria/fieldguide/vic/fork/model/Detail.java
+++ src/main/java/au/com/museumvictoria/fieldguide/vic/fork/model/Detail.java
@@ -4,233 +4,232 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Detail {
-	private String description;
-	private String bite;
-	private String biology;
-	private String diet;
-	private String habitat;
-	private String nativeStatus;
-	private String distinctive;
-	private String distribution;
-	private String[] depth;
-	private String[] location;	
-	private String butterflyStart;
-	private String butterflyEnd;
-	private boolean isCommercial;
-	private String taxaPhylum;
-	private String taxaClass;
-	private String taxaOrder;
-	private String taxaFamily;
-	private String taxaGenus;
-	private String taxaSpecies;
-	private String taxaSubSpecies;
-	private String commonNames;
-	private String otherNames;
-	private String distributionMap; 
-	
-	public Detail() { }
+  private String description;
+  private String bite;
+  private String biology;
+  private String diet;
+  private String habitat;
+  private String nativeStatus;
+  private String distinctive;
+  private String distribution;
+  private String[] depth;
+  private String[] location;
+  private String butterflyStart;
+  private String butterflyEnd;
+  private boolean isCommercial;
+  private String taxaPhylum;
+  private String taxaClass;
+  private String taxaOrder;
+  private String taxaFamily;
+  private String taxaGenus;
+  private String taxaSpecies;
+  private String taxaSubSpecies;
+  private String commonNames;
+  private String otherNames;
+  private String distributionMap;
 
-	public String getDescription() {
-		return description;
-	}
+  public Detail() { }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public String getBite() {
-		if (bite == null) bite = "";
-		return bite;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-	public void setBite(String bite) {
-		this.bite = bite;
-	}
+  public String getBite() {
+    if (bite == null) bite = "";
+    return bite;
+  }
 
-	public String getBiology() {
-		return biology;
-	}
+  public void setBite(String bite) {
+    this.bite = bite;
+  }
 
-	public void setBiology(String biology) {
-		this.biology = biology;
-	}
+  public String getBiology() {
+    return biology;
+  }
 
-	public String getDiet() {
-		if (diet == null) diet = ""; 
-		return diet;
-	}
+  public void setBiology(String biology) {
+    this.biology = biology;
+  }
 
-	public void setDiet(String diet) {
-		this.diet = diet;
-	}
+  public String getDiet() {
+    if (diet == null) diet = "";
+    return diet;
+  }
 
-	public String getHabitat() {
-		return habitat;
-	}
+  public void setDiet(String diet) {
+    this.diet = diet;
+  }
 
-	public void setHabitat(String habitat) {
-		this.habitat = habitat;
-	}
+  public String getHabitat() {
+    return habitat;
+  }
 
-	public String getNativeStatus() {
-		return nativeStatus;
-	}
+  public void setHabitat(String habitat) {
+    this.habitat = habitat;
+  }
 
-	public void setNativeStatus(String nativeStatus) {
-		this.nativeStatus = nativeStatus;
-	}
+  public String getNativeStatus() {
+    return nativeStatus;
+  }
 
-	public String getDistinctive() {
-		//if (distinctive == null) distinctive = "";
-		return distinctive;
-	}
+  public void setNativeStatus(String nativeStatus) {
+    this.nativeStatus = nativeStatus;
+  }
 
-	public void setDistinctive(String distinctive) {
-		this.distinctive = distinctive;
-	}
+  public String getDistinctive() {
+    //if (distinctive == null) distinctive = "";
+    return distinctive;
+  }
 
-	public String getDistribution() {
-		return distribution;
-	}
+  public void setDistinctive(String distinctive) {
+    this.distinctive = distinctive;
+  }
 
-	public void setDistribution(String distribution) {
-		this.distribution = distribution;
-	}
+  public String getDistribution() {
+    return distribution;
+  }
 
-	public String[] getDepth() {
-		return depth;
-	}
+  public void setDistribution(String distribution) {
+    this.distribution = distribution;
+  }
 
-	public void setDepth(String[] depth) {
-		this.depth = depth;
-	}
+  public String[] getDepth() {
+    return depth;
+  }
 
-	public String[] getLocation() {
-		return location;
-	}
+  public void setDepth(String[] depth) {
+    this.depth = depth;
+  }
 
-	public void setLocation(String[] location) {
-		this.location = location;
-	}
+  public String[] getLocation() {
+    return location;
+  }
 
-	public String getTaxaPhylum() {
-		return taxaPhylum;
-	}
+  public void setLocation(String[] location) {
+    this.location = location;
+  }
 
-	public void setTaxaPhylum(String taxaPhylum) {
-		this.taxaPhylum = taxaPhylum;
-	}
+  public String getTaxaPhylum() {
+    return taxaPhylum;
+  }
 
-	public String getTaxaClass() {
-		return taxaClass;
-	}
+  public void setTaxaPhylum(String taxaPhylum) {
+    this.taxaPhylum = taxaPhylum;
+  }
 
-	public void setTaxaClass(String taxaClass) {
-		this.taxaClass = taxaClass;
-	}
+  public String getTaxaClass() {
+    return taxaClass;
+  }
 
-	public String getTaxaOrder() {
-		return taxaOrder;
-	}
+  public void setTaxaClass(String taxaClass) {
+    this.taxaClass = taxaClass;
+  }
 
-	public void setTaxaOrder(String taxaOrder) {
-		this.taxaOrder = taxaOrder;
-	}
+  public String getTaxaOrder() {
+    return taxaOrder;
+  }
 
-	public String getTaxaFamily() {
-		return taxaFamily;
-	}
+  public void setTaxaOrder(String taxaOrder) {
+    this.taxaOrder = taxaOrder;
+  }
 
-	public void setTaxaFamily(String taxaFamily) {
-		this.taxaFamily = taxaFamily;
-	}
+  public String getTaxaFamily() {
+    return taxaFamily;
+  }
 
-	public String getTaxaGenus() {
-		return taxaGenus;
-	}
+  public void setTaxaFamily(String taxaFamily) {
+    this.taxaFamily = taxaFamily;
+  }
 
-	public void setTaxaGenus(String taxaGenus) {
-		this.taxaGenus = taxaGenus;
-	}
+  public String getTaxaGenus() {
+    return taxaGenus;
+  }
 
-	public String getTaxaSpecies() {
-		return taxaSpecies;
-	}
+  public void setTaxaGenus(String taxaGenus) {
+    this.taxaGenus = taxaGenus;
+  }
 
-	public void setTaxaSpecies(String taxaSpecies) {
-		this.taxaSpecies = taxaSpecies;
-	}
+  public String getTaxaSpecies() {
+    return taxaSpecies;
+  }
 
-	public String getTaxaSubSpecies() {
-		return taxaSubSpecies;
-	}
+  public void setTaxaSpecies(String taxaSpecies) {
+    this.taxaSpecies = taxaSpecies;
+  }
 
-	public void setTaxaSubSpecies(String taxaSubSpecies) {
-		this.taxaSubSpecies = taxaSubSpecies;
-	}
+  public String getTaxaSubSpecies() {
+    return taxaSubSpecies;
+  }
 
-	public String getCommonNames() {
-		return commonNames;
-	}
+  public void setTaxaSubSpecies(String taxaSubSpecies) {
+    this.taxaSubSpecies = taxaSubSpecies;
+  }
 
-	public void setCommonNames(String commonNames) {
-		this.commonNames = commonNames;
-	}
+  public String getCommonNames() {
+    return commonNames;
+  }
 
-	public String getOtherNames() {
-		if (otherNames == null) otherNames = ""; 
-		return otherNames;
-	}
+  public void setCommonNames(String commonNames) {
+    this.commonNames = commonNames;
+  }
 
-	public void setOtherNames(String otherNames) {
-		this.otherNames = otherNames;
-	}
+  public String getOtherNames() {
+    if (otherNames == null) otherNames = "";
+    return otherNames;
+  }
 
-	public String getButterflyStart() {
-		return butterflyStart;
-	}
+  public void setOtherNames(String otherNames) {
+    this.otherNames = otherNames;
+  }
 
-	public void setButterflyStart(String butterflyStart) {
-		this.butterflyStart = butterflyStart;
-	}
+  public String getButterflyStart() {
+    return butterflyStart;
+  }
 
-	public String getButterflyEnd() {
-		return butterflyEnd;
-	}
+  public void setButterflyStart(String butterflyStart) {
+    this.butterflyStart = butterflyStart;
+  }
 
-	public void setButterflyEnd(String butterflyEnd) {
-		this.butterflyEnd = butterflyEnd;
-	}
+  public String getButterflyEnd() {
+    return butterflyEnd;
+  }
 
-	public boolean isCommercial() {
-		return isCommercial;
-	}
+  public void setButterflyEnd(String butterflyEnd) {
+    this.butterflyEnd = butterflyEnd;
+  }
 
-	public void setCommercial(boolean isCommercial) {
-		this.isCommercial = isCommercial;
-	}
+  public boolean isCommercial() {
+    return isCommercial;
+  }
 
-	public String getDistributionMap() {
-		return distributionMap;
-	}
+  public void setCommercial(boolean isCommercial) {
+    this.isCommercial = isCommercial;
+  }
 
-	public void setDistributionMap(String distributionMap) {
-		this.distributionMap = distributionMap;
-	}
+  public String getDistributionMap() {
+    return distributionMap;
+  }
 
-	@Override
-	public String toString() {
-		return "Detail [description=" + description + ", bite=" + bite
-				+ ", biology=" + biology + ", diet=" + diet + ", habitat="
-				+ habitat + ", nativeStatus=" + nativeStatus + ", distinctive="
-				+ distinctive + ", distribution=" + distribution
-				+ ", butterflyStart=" + butterflyStart + ", butterflyEnd="
-				+ butterflyEnd + ", isCommercial=" + isCommercial
-				+ ", taxaPhylum=" + taxaPhylum + ", taxaClass=" + taxaClass
-				+ ", taxaOrder=" + taxaOrder + ", taxaFamily=" + taxaFamily
-				+ ", taxaGenus=" + taxaGenus + ", taxaSpecies=" + taxaSpecies
-				+ ", commonNames=" + commonNames + ", otherNames=" + otherNames
-				+ ", distributionMap=" + distributionMap + "]";
-	}
+  public void setDistributionMap(String distributionMap) {
+    this.distributionMap = distributionMap;
+  }
 
+  @Override
+  public String toString() {
+    return "Detail [description=" + description + ", bite=" + bite
+        + ", biology=" + biology + ", diet=" + diet + ", habitat="
+        + habitat + ", nativeStatus=" + nativeStatus + ", distinctive="
+        + distinctive + ", distribution=" + distribution
+        + ", butterflyStart=" + butterflyStart + ", butterflyEnd="
+        + butterflyEnd + ", isCommercial=" + isCommercial
+        + ", taxaPhylum=" + taxaPhylum + ", taxaClass=" + taxaClass
+        + ", taxaOrder=" + taxaOrder + ", taxaFamily=" + taxaFamily
+        + ", taxaGenus=" + taxaGenus + ", taxaSpecies=" + taxaSpecies
+        + ", commonNames=" + commonNames + ", otherNames=" + otherNames
+        + ", distributionMap=" + distributionMap + "]";
+  }
 }
