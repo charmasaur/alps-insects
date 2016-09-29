f7bafbffe903360878ecdab73d5ee0d2894067b9
Harry Slatyer <harry.slatyer@gmail.com>
2016-09-29 21:29:56 +1000

    Remove unnecessary Handler in splash

--- src/main/java/com/github/charmasaur/alpsinsects/ui/SplashActivity.java
+++ src/main/java/com/github/charmasaur/alpsinsects/ui/SplashActivity.java
@@ -2,7 +2,6 @@ package com.github.charmasaur.alpsinsects.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import com.github.charmasaur.alpsinsects.R;
@@ -15,21 +14,18 @@ public final class SplashActivity extends AppCompatActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    final Handler handler = new Handler();
     // Load the database on a background thread.
     new Thread(new Runnable() {
       public void run() {
         Log.i(TAG, "Opening database.");
         FieldGuideDatabase.getInstance(getApplicationContext()).open();
 
-        handler.postDelayed(new Runnable() {public void run() {
         Log.i(TAG, "Database opened, starting main activity.");
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
 
         finish();
-        }}, 0);
       }
     }).start();
   }
